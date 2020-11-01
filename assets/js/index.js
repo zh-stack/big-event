10a9fa338215c63dcb2aec2128d5c8e219520fcc
ykd <386337563@qq.com>
2020-11-01 11:53:27 +0800

    用户信息项目完成

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+$(function(){
+    getUserInfo();
+    var layer = layui.layer;
+    $('#tuichu').on('click',function(){
+        layer.confirm('确定退出登录?',{ icon: 3, title: '提示' }, 
+        function(index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token');
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html';
+            // 关闭 confirm 询问框
+            layer.close(index);
+        })
+    })
+})
+//获取用户信息发起的ajax
+function getUserInfo(){
+    $.ajax({
+        type:'GET',
+        url:'/my/userinfo',
+        headers:{
+            Authorization: localStorage.getItem('token')||''
+        },
+        success:function(res){
+            console.log(res);
+            if(res.status !== 0){
+                return layui.layer.msg('获取用户信息失败！');
+            }
+            randerUserAvatar(res.data);
+        },
+        complete:function(res){
+            if(res.responseJSON.status === 1&&res.responseJSON.message === '身份认证失败！'){
+                localStorage.removeItem('token');
+                location.href = 'login.html';
+            }
+        }
+    })
+}
+function randerUserAvatar(user) {
+    var name = user.nickname||user.username;
+    $('#welcome').html('欢迎&nbsp&nbsp'+name);
+    if(user.user_pic !== null){
+        //用户图片
+        $('.layui-nav-img').attr('src',user.user_pic).show();
+        $('.text-avatar').hide();
+    }else{
+        //文本图片
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
+}
